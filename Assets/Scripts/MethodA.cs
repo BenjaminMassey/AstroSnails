c3581df06e381d68f327df3e0a81bb71d5a714ea
Ben Massey <benjamin.w.massey@gmail.com>
2020-07-29 13:01:14 -0700

    Initial commit

---
+++ Assets/Scripts/MethodA.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MethodA : MonoBehaviour
+{
+    private float MoveSpeed = 8.0f;
+    private bool debug_on = false;
+
+    private Text t;
+    private GameObject world;
+
+    private float my_height;
+    private float world_height;
+
+    private Vector3 start_pos;
+    private Quaternion start_rot;
+
+    private GameObject debug_spot;
+
+    private Vector3 radius_spot;
+    private Vector3 movement_addition;
+
+    private Vector3 last_rot;
+    private Vector3 curr_rot;
+
+    private bool swapped;
+
+    private void Start()
+    {
+        t = GameObject.Find("Text").GetComponent<Text>();
+        world = GameObject.Find("Sphere");
+
+        my_height = transform.localScale.y / 2.0f;
+        world_height = world.transform.localScale.y / 2.0f;
+
+        start_pos = transform.position;
+        start_rot = transform.rotation;
+
+        if (debug_on)
+        {
+            debug_spot = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            debug_spot.transform.localScale *= 0.25f;
+            debug_spot.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.yellow);
+            debug_spot.name = "debug_spot";
+        }
+        else
+        {
+            t.text = "";
+        }
+
+        radius_spot = Vector3.zero;
+        movement_addition = Vector3.zero;
+
+        last_rot = transform.rotation.eulerAngles;
+        curr_rot = transform.rotation.eulerAngles;
+
+        swapped = false;
+    }
+
+    private void Update()
+    {
+        transform.LookAt(world.transform);
+        RadiusSet();
+        SwapFix();
+        Movement();
+        transform.position = radius_spot + movement_addition;
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        { 
+            transform.position = start_pos;
+            transform.rotation = start_rot;
+        }
+    }
+
+    private void FixedUpdate() {}
+
+    private void SwapFix()
+    {
+        last_rot = curr_rot;
+        curr_rot = transform.rotation.eulerAngles;
+
+        if (debug_on)
+        {
+            Debug.Log("last_rot: " + last_rot + " | curr_rot: " + curr_rot);
+        }
+
+        float result = Mathf.Abs(curr_rot.y - last_rot.y);
+        
+        if (result > 170.0f && result < 190.0f)
+        {
+            Debug.Log("SWAP!!!");
+            swapped = !swapped;
+        }
+    }
+
+    private void RadiusSet()
+    {
+        Vector3 p = transform.position;
+        float r = Mathf.Sqrt(Mathf.Pow(p.x, 2.0f) + Mathf.Pow(p.y, 2.0f) + Mathf.Pow(p.z, 2.0f));
+        if (r == world_height) { return; }
+        bool inside = r < world_height;
+
+        int mask = inside ? 1 << 11 : 1 << 10;
+        Vector3 direction = inside ? transform.forward * -1.0f : transform.forward;
+        
+        RaycastHit rch;
+        bool hit = Physics.Raycast(transform.position, direction, out rch, Mathf.Infinity, mask);
+        if (hit)
+        {
+            if (debug_on)
+            {
+                t.text = "Raycast\n" + rch.collider.gameObject.name + "\n" + Mathf.Round(rch.distance * 100.0f) / 100.0f + "m";
+                debug_spot.transform.position = rch.point;
+            }
+            radius_spot = rch.point;
+        }
+    }
+
+    private void Movement()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        float XFACTOR = swapped ? -1.0f : 1.0f;
+        Vector3 move = (transform.right * input.x) + (transform.up * input.y * XFACTOR);
+        move *= Time.deltaTime;
+        move *= MoveSpeed;
+
+        movement_addition = move;
+    }
+}
---
+++ Assets/Scripts/MethodB.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MethodB : MonoBehaviour
+{
+    private float run_speed = 45.0f;
+    private float turn_speed = 125.0f;
+    private float jump_amount = 0.15f;
+    private float fall_speed = 0.25f;
+
+    private bool gravity_on;
+    private bool jumpable;
+
+    private GameObject player;
+    private float player_local_z_start;
+
+    private Text t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gravity_on = true;
+        jumpable = true;
+        player = transform.GetChild(0).transform.gameObject;
+        player_local_z_start = player.transform.localPosition.z;
+        t = GameObject.Find("Text").GetComponent<Text>();
+        t.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(/*Input.GetAxis("Vertical") * */Time.deltaTime * run_speed * -1.0f,
+                         0.0f,
+                         Input.GetAxis("Horizontal") * Time.deltaTime * turn_speed);
+
+        //t.text = player.transform.localPosition.ToString();
+
+        if (jumpable && Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine("Jump");
+        }
+
+        if (player.transform.localPosition.z > player_local_z_start)
+        {
+            if (gravity_on)
+            {
+                RaiseLowerPlayer(-1.0f * fall_speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            jumpable = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void RaiseLowerPlayer(float amount)
+    {
+        Vector3 lp = player.transform.localPosition;
+        player.transform.localPosition = new Vector3(lp.x, lp.y, lp.z + amount);
+    }
+
+    IEnumerator Jump()
+    {
+        jumpable = false;
+        gravity_on = false;
+        float seconds = 0.5f;
+        float iters = 50 * seconds;
+        for (int i = 0; i < iters; i++)
+        {
+            RaiseLowerPlayer(jump_amount / iters);
+            yield return new WaitForFixedUpdate();
+        }
+        gravity_on = true;
+    }
+
+}
