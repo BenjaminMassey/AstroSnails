83172dd08c680d7c1021e4d3c5437f2fc2224aa4
Ben Massey <benjamin.w.massey@gmail.com>
2020-08-10 00:59:05 -0700

    Started char customization: simply have UI with correct display and button input

---
+++ Assets/Scripts/CustomLevelDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomLevelDisplay : MonoBehaviour
+{
+    List<GameObject> levels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levels = new List<GameObject>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            levels.Add(transform.GetChild(i).gameObject);
+        }
+    }
+
+    public void Display(int num)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            levels[i].GetComponent<Image>().color = Color.yellow;
+        }
+        for (int i = num; i < levels.Count; i++)
+        {
+            levels[i].GetComponent<Image>().color = Color.white;
+        }
+    }
+}
---
+++ Assets/Scripts/Customization.cs
@@ -0,0 +1,81 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Customization : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject points_left_obj;
+    [SerializeField]
+    private GameObject speed_levels_obj;
+    [SerializeField]
+    private GameObject turn_levels_obj;
+    [SerializeField]
+    private GameObject cap_levels_obj;
+    [SerializeField]
+    private GameObject regen_levels_obj;
+
+    private int point_max = 5;
+    private int points_left_start = 10;
+
+    private int points_left;
+
+    private Dictionary<string, int> points;
+    private Dictionary<string, GameObject> level_objs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points_left = points_left_start;
+        UpdatePointsLeftText();
+
+        points = new Dictionary<string, int>();
+        points.Add("speed", 0);
+        points.Add("turn", 0);
+        points.Add("cap", 0);
+        points.Add("regen", 0);
+
+        level_objs = new Dictionary<string, GameObject>();
+        level_objs.Add("speed", speed_levels_obj);
+        level_objs.Add("turn", turn_levels_obj);
+        level_objs.Add("cap", cap_levels_obj);
+        level_objs.Add("regen", regen_levels_obj);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void point_up(string key)
+    {
+        if (points[key] < point_max && points_left > 0)
+        {
+            points[key]++;
+            level_objs[key].GetComponent<CustomLevelDisplay>().Display(points[key]);
+
+            points_left--;
+            UpdatePointsLeftText();
+        }
+    }
+
+    public void point_down(string key)
+    {
+        if (points[key] > 0)
+        {
+            points[key]--;
+            level_objs[key].GetComponent<CustomLevelDisplay>().Display(points[key]);
+
+            points_left++;
+            UpdatePointsLeftText();
+        }
+    }
+
+    private void UpdatePointsLeftText()
+    {
+        points_left_obj.GetComponent<Text>().text = "Points Left: " + points_left.ToString();
+    }
+}
