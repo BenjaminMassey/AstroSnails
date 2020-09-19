daa7a05ea36e70f1c9d97eb8d7b3dc35c5894c47
BenjaminMassey <benjamin.w.massey@gmail.com>
2020-09-18 23:59:35 -0700

    Music :)

---
+++ Assets/Scripts/GameMusic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusic : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource[] game_music_sources;
+
+    void Awake()
+    {
+        int choice = Random.Range(0, game_music_sources.Length);
+        game_music_sources[choice].loop = true;
+        game_music_sources[choice].Play();
+    }
+}
---
+++ Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusic : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource menu_audio_source;
+    
+    void Awake()
+    {
+        menu_audio_source.loop = true;
+        menu_audio_source.Play();
+    }
+}
