bac39989bfc583586166e4030296c63859c4061b
BenjaminMassey <benjamin.w.massey@gmail.com>
2020-09-27 18:01:26 -0700

    Controls page

---
+++ Assets/Scripts/ControlsNav.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsNav : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject first_screen;
+
+    [SerializeField]
+    private GameObject controls_screen;
+
+    public void GoToControlsScreen()
+    {
+        first_screen.SetActive(false);
+        controls_screen.SetActive(true);
+    }
+
+    public void GoToFirstScreen()
+    {
+        controls_screen.SetActive(false);
+        first_screen.SetActive(true);
+    }
+}
